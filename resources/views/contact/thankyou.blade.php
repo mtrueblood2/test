3ed568c210820a2c957710c6fc7d87ffe010aec8
Matt Trueblood <mtrueblood3@gmail.com>
2019-01-24 18:32:11 -0500

    Adding in the home and thank you pages

--- resources/views/contact/thankyou.blade.php
+++ resources/views/contact/thankyou.blade.php
@@ -2,6 +2,12 @@
 
 @section('content')
 
-<!-- add in thank you content in here -->
+  <div class="jumbotron text-center">
+    <div class="container">
+      <h1 class="jumbotron-heading">Thanks for your message!</h1>
+      <p class="lead text-muted">I love getting in touch with people and I will try to reach out to you soon.</p>
+    </div>
+  </div>
+
 
 @endsection
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,6 +2,20 @@
 
 @section('content')
 
-<!-- add in welcome page content here -->
+	<style>
+	  body{
+	  	background-color:#343a40;
+	  }
+    </style>
+  	<div class="container text-light w-90 mt-5">
+	    <h1 class="cover-heading mt-8">Hello, I'm Matt.</h1>
+	    <p class="lead w-75">I'm a programmer, but what I really love to work on is solutions to problems. Whether it's finding a way to streamline a business process, make a system trackable, or just give people more control over their day I love being able to improve things. Programming is my way to contribute and I take it very seriously. I love writing clean, maintainable code and I am comfortable using PHP, C#, Java, and a whole host of other languages and platforms. I'm comfortable working on the command line remotely and think that the cloud is going to continue to allow us to revolutionize.</p>
+	    <p class="lead w-75">When I'm not programming I can usually be found enjoying time with my family, playing guitar, or video games. I love travelling with my family and since we love the outdoors we usually find ourselves in national parks. As a former restaurant owner I love to cook, and my wife and I are always trying to cook food we have never had before.</p>
+	    <p class="lead w-75">I'd love to hear from you so please click the button below to drop me a line!</p>
+	    <p class="lead">
+	      <a href="/contact/create" class="btn btn-lg btn-primary">Contact Me</a>
+	    </p>
+	</div>
+
 
 @endsection
