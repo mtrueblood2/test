284b0ed20460960e308e01421f9b73e6dc5f93ab
Matt Trueblood <mtrueblood3@gmail.com>
2019-01-23 22:24:27 -0500

    Initial model, migration, controller
    Only simple config is completed

---
+++ app/Http/Controllers/ContactController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ContactController extends Controller
+{
+    public function store(Request $request){
+
+    	// TODO validate input
+
+    	// Build up object
+
+    	// Save object
+
+    	// Redirect to thank you page
+
+    }
+}
